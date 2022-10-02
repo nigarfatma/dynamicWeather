d979fcdfd5be79664ac834117edc8e386f624725
nigarfatma180 <nigarfatma180@gmail.com>
2022-10-02 17:48:06 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const http = require("http");
+const fs = require("fs");
+var requests = require("requests");
+
+
+const homeFile = fs.readFileSync("home.html", "utf-8");
+
+
+const replaceVal = (tempVal, orgVal) => {
+  let temperature = tempVal.replace("{%tempval%}", orgVal.main.temp);
+  temperature = temperature.replace("{%tempmin%}", orgVal.main.temp_min);
+  temperature = temperature.replace("{%tempmax%}", orgVal.main.temp_max);
+  temperature = temperature.replace("{%location%}", orgVal.name);
+  temperature = temperature.replace("{%country%}", orgVal.sys.country);
+  temperature = temperature.replace("{%tempstatus%}", orgVal.weather[0].main);
+
+  return temperature;
+};
+
+const server = http.createServer((req, res) => {
+  if (req.url == "/") {
+    requests(
+       " https://api.openweathermap.org/data/2.5/weather?q=Asansol&appid=016dc90a1f1e98d6d2d26d3c82eddee7"
+    )
+      .on("data", (chunk) => {
+        const objdata = JSON.parse(chunk);
+        const arrData = [objdata];
+        // console.log(arrData[0].main.temp);
+        const realTimeData = arrData
+          .map((val) => replaceVal(homeFile, val))
+          .join("");
+        res.write(realTimeData);
+        // console.log(realTimeData);
+      })
+      .on("end", (err) => {
+        if (err) return console.log("connection closed due to errors", err);
+        res.end();
+      });
+  } else {
+    res.end("File not found");
+  }
+});
+
+server.listen(3000, "127.0.0.1");
